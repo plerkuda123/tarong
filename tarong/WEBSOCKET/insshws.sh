722d70031b2e215263ecaeb0ae6689c15ef88110
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-03 13:48:55 +0800

    Update insshws.sh

--- tarong/WEBSOCKET/insshws.sh
+++ tarong/WEBSOCKET/insshws.sh
@@ -18,7 +18,7 @@ wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Tarap-Kuhin
 chmod +x /usr/local/bin/ws-dropbear
 
 # Installing Service
-cat > /etc/systemd/system/ws-nontls.service << END
+cat > /etc/systemd/system/ws-dropbear << END
 [Unit]
 Description=Python Proxy Mod By TARAP KUHING
 Documentation=https://t.me/Hendra2012
