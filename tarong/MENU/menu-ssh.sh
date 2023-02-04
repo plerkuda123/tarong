8063153a4256bddcc57454ff692667e136cb1737
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-04 22:51:37 +0800

    Update menu-ssh.sh

--- tarong/MENU/menu-ssh.sh
+++ tarong/MENU/menu-ssh.sh
@@ -560,7 +560,7 @@ ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 OhpSSH=`cat /root/log-install.txt | grep -w "OHP SSH" | cut -d: -f2 | awk '{print $1}'`
 
 
-Login=alexxa`</dev/urandom tr -dc X-Z0-9 | head -c4`
+Login=♧Tarap-Kuhing♧`</dev/urandom tr -dc X-Z0-9 | head -c4`
 hari="1"
 Pass=1
 echo Ping Host &> /dev/null
