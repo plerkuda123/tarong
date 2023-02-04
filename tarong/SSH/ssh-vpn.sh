6ad660c7ae0f05bd345af30eee32112e75dc0c3a
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-05 00:15:15 +0800

    Update ssh-vpn.sh

--- tarong/SSH/ssh-vpn.sh
+++ tarong/SSH/ssh-vpn.sh
@@ -340,10 +340,13 @@ wget -O speedtest "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/ta
 wget -O xp "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/SSH/xp.sh"
 wget -O auto-set "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/XRAY/auto-set.sh"
 wget -O menu-update "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-update"
+wget -O slhost "https://raw.githubusercontent.com/Tarap-Kuhing/SLDNS/main/slowdns/install-sldns"
+
 chmod +x speedtest
 chmod +x xp
 chmod +x auto-set
-chmod +x menu-update 
+chmod +x menu-update
+chmod +x slhost
 cd
 
 
