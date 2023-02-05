11f607fc46ae8581670ca350b38128070a4c11a0
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-05 10:50:17 +0800

    Update slhost.sh

--- tarong/SSH/slhost.sh
+++ tarong/SSH/slhost.sh
@@ -25,17 +25,41 @@ mkdir -p /etc/xray
 mkdir -p /etc/v2ray
 echo "$SUB_DOMAIN" >> /etc/v2ray/domain
 #
-DOMAIN=sg3.tarong.my.id
+DOMAIN=tarong.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
-SUB_DOMAIN=${sub}.sg3.tarong.my.id
-NS_DOMAIN=${sub}.sg3.tarong.my.id
+SUB_DOMAIN=${sub}.tarong.my.id
+NS_DOMAIN=${sub}.tarong.my.id
 echo "IP=""$SUB_DOMAIN" >> /var/lib/ipvps.conf
 echo "$NS_DOMAIN" >> /root/nsdomain
-#CF_ID=merahjambo@gmail.com
-#CF_KEY=86431de017f7bf317c3960061da2f87c8effb
+CF_ID=merahjambo@gmail.com
+CF_KEY=86431de017f7bf317c3960061da2f87c8effb
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 echo "Updating DNS NS for ${NS_DOMAIN}..."
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
      -H "X-Auth-Email: ${CF_ID}" \
