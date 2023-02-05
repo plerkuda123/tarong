7f4d9b784975672f654b79278377ca0ebc6b727e
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-05 10:21:04 +0800

    Create slhost.sh

---
+++ tarong/SSH/slhost.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+colornow=$(cat /etc/tarap/theme/color.conf)
+NC="\e[0m"
+RED="\033[0;31m"
+COLOR1="$(cat /etc/tarap/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/tarap/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+WH='\033[1;37m'
+###########- END COLOR CODE -##########
+MYIP=$(wget -qO- icanhazip.com);
+apt install jq curl -y
+rm -f /root/domain
+rm -f /etc/v2ray/domain
+rm -f /etc/xray/domain
+rm -rf /etc/xray/domain
+rm -rf /root/nsdomain
+rm -rf /var/lib/ipvps.conf
+rm nsdomain
+rm domain
+mkdir -p /usr/bin/xray
+mkdir -p /usr/bin/v2ray
+mkdir -p /etc/xray
+mkdir -p /etc/v2ray
+echo "$SUB_DOMAIN" >> /etc/v2ray/domain
+#
+DOMAIN=sg3.tarong.my.id
+sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
+subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
+SUB_DOMAIN=${sub}.sg3.tarong.my.id
+NS_DOMAIN=${sub}.sg3.tarong.my.id
+echo "IP=""$SUB_DOMAIN" >> /var/lib/ipvps.conf
+echo "$NS_DOMAIN" >> /root/nsdomain
+#CF_ID=merahjambo@gmail.com
+#CF_KEY=86431de017f7bf317c3960061da2f87c8effb
+set -euo pipefail
+IP=$(wget -qO- icanhazip.com);
+echo "Updating DNS NS for ${NS_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":false}')
+rm -rf /etc/xray/domain
+rm -rf /root/nsdomain
+echo "Host : $SUB_DOMAIN"
+echo $SUB_DOMAIN > /root/domain
+echo "Host SlowDNS : $NS_DOMAIN"
+echo "$NS_DOMAIN" >> /root/nsdomain
+echo "$SUB_DOMAIN" >> /etc/xray/domain
+cd
