be41f45403c5879829a0b2b9220f536b6e9b6acf
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-05 14:13:45 +0800

    Update slhost.sh

--- tarong/SSH/slhost.sh
+++ tarong/SSH/slhost.sh
@@ -4,11 +4,7 @@ apt install jq curl -y
 #============
 DOMAIN=tarong.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
 dns=${sub}.tarong.my.id
-NS_DOMAIN=${subsl}.tarong.my.id
-echo "IP=""$dns" >> /var/lib/ipvps.conf
-echo "$NS_DOMAIN" >> /root/nsdomain
 CF_ID=merahjambo@gmail.com
 CF_KEY=86431de017f7bf317c3960061da2f87c8effb
 set -euo pipefail
@@ -37,40 +33,11 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}')
-echo "Updating DNS NS for ${NS_DOMAIN}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-if [[ "${#RECORD}" -le 10 ]]; then
-     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":false}' | jq -r .result.id)
-fi
-
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":false}')
-rm -rf /etc/xray/domain
-rm -rf /root/nsdomain
-echo "Host : $dns"
 echo "$dns" > /root/domain
 echo "$dns" > /root/scdomain
-echo "Host SlowDNS : $NS_DOMAIN"
-echo "$NS_DOMAIN" >> /root/nsdomain
-echo "$dns" >> /etc/xray/domain
 echo "$dns" > /etc/xray/domain
 echo "$dns" > /etc/v2ray/domain
 echo "$dns" > /etc/v2ray/scdomain
 echo "$dns" > /etc/xray/scdomain
+echo "IP=$dns" > /var/lib/ipvps.conf
 cd
