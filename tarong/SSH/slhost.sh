07591dc77390e0215fed42d9dc483d520326577c
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-05 11:19:09 +0800

    Update slhost.sh

--- tarong/SSH/slhost.sh
+++ tarong/SSH/slhost.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
-colornow=$(cat /etc/tarap/theme/color.conf)
-NC="\e[0m"
-RED="\033[0;31m"
-COLOR1="$(cat /etc/tarap/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
-COLBG1="$(cat /etc/tarap/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
-WH='\033[1;37m'
-###########- END COLOR CODE -##########
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
 rm -f /root/domain
