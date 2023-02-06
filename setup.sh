23823aa6298af86038cd9d7a3e261f27f3d6d914
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-06 19:26:11 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -147,8 +147,8 @@ clear
 fi
 fi
 echo ""
-#wget -q https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/dependencies.sh;chmod +x dependencies.sh;./dependencies.sh
-#rm dependencies.sh
+wget -q https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/dependencies.sh;chmod +x dependencies.sh;./dependencies.sh
+rm dependencies.sh
 clear
 wget https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/SSH/Tarap-Kuhing.sh && chmod +x Tarap-Kuhing.sh && ./Tarap-Kuhing.sh
 sleep 20
@@ -207,7 +207,7 @@ blue
 EOF
 clear
 #Install Tarap_Kuhing
-wget https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/SSH/Tarap_Kuhing.sh && chmod +x Tarap_Kuhing.sh && ./Tarap_Kuhing.sh
+#wget https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/SSH/Tarap_Kuhing.sh && chmod +x Tarap_Kuhing.sh && ./Tarap_Kuhing.sh
 sleep 5
 clear
 #install ssh ovpn
