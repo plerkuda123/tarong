db12622ac3bc13f31687a44bb7eede6d7a625db8
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-07 14:16:01 +0800

    Update Tarap-Kuhing.sh

--- tarong/SSH/Tarap-Kuhing.sh
+++ tarong/SSH/Tarap-Kuhing.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
+rm -rf /root/nsdomain
+rm -rf /var/lib/ipvps.conf
+rm nsdomain
 #============
 DOMAIN=kuhing.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-#subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
+subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 dns=${sub}.kuhing.my.id
-#NS_dns=${subsl}.kuhing.my.id
+NS_dns=${subsl}.kuhing.my.id
 CF_ID=merahjambo@gmail.com
 CF_KEY=86431de017f7bf317c3960061da2f87c8effb
 set -euo pipefail
@@ -37,11 +40,11 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}')
 echo "$dns" > /root/domain
 echo "$dns" > /root/scdomain
-#echo "$NS_dns" > /root/nsdomain
+echo "$NS_dns" > /root/nsdomain
 echo "$dns" > /etc/xray/domain
 echo "$dns" > /etc/v2ray/domain
 echo "$dns" > /etc/v2ray/scdomain
 echo "$dns" > /etc/xray/scdomain
-#echo "$NS_dns" > /etc/xray/nsdomain
+echo "$NS_dns" > /etc/xray/nsdomain
 echo "IP=$dns" > /var/lib/ipvps.conf
 cd
