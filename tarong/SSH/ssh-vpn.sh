9080dfc8ff5c8cb49d2011d6b4e586b17bcf3884
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-09 09:08:10 +0800

    Update ssh-vpn.sh

--- tarong/SSH/ssh-vpn.sh
+++ tarong/SSH/ssh-vpn.sh
@@ -340,11 +340,12 @@ wget -O speedtest "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/ta
 wget -O xp "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/SSH/xp.sh"
 wget -O auto-set "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/XRAY/auto-set.sh"
 wget -O menu-update "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-update.sh"
-
+wget -O backup "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/SSH/backup.sh"
 chmod +x speedtest
 chmod +x xp
 chmod +x auto-set
 chmod +x menu-update
+chmod +x backup
 cd
 
 
@@ -430,6 +431,8 @@ rm -f /root/cert.pem
 rm -f /root/ssh-vpn.sh
 rm -f /root/bbr.sh
 rm -f /root/menu-update
+rm -f /root/backup
+
 
 # finihsing
 clear
