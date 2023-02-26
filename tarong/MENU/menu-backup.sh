4e4abf74777d7d9918b3dd411e45609cb37956ac
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-26 10:45:29 +0800

    Update menu-backup.sh

--- tarong/MENU/menu-backup.sh
+++ tarong/MENU/menu-backup.sh
@@ -144,6 +144,8 @@ Save_And_Exit () {
     git branch -M main &> /dev/null
     git remote add origin https://github.com/jambanbkn/userbackup.git
     git push -f https://github.com/jambanbkn/userbackup.git &> /dev/null
+    Username for 'https://github.com': jambanbkn
+    Password for 'https://jambanbkn@github.com': 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
 }
 
 if [ ! -d "/root/user-backup/" ]; then
