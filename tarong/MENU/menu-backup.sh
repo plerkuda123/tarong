039d2e6eccaf702a3fcaa3408661410867ccac05
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-03 21:04:19 +0800

    Update menu-backup.sh

--- tarong/MENU/menu-backup.sh
+++ tarong/MENU/menu-backup.sh
@@ -286,7 +286,7 @@ echo -e ""
 case $opt in
 01 | 1) clear ; backup ;;
 02 | 2) clear ; restore ;;
-#03 |3) clear ; autobackup ;;
+03 |3) clear ; autobackup ;;
 #04 |4) clear ; menu4 ;;
 00 | 0) clear ; menu ;;
 *) clear ; menu-backup ;;
