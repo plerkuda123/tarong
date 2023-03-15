3b99e6f46404ee97b1d26870253d75fcfff0f4c0
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-15 14:13:58 +0800

    Update menu-backup.sh

--- tarong/MENU/menu-backup.sh
+++ tarong/MENU/menu-backup.sh
@@ -91,21 +91,15 @@ echo -e "[ ${green}INFO${NC} ] Processing... "
 mkdir -p /root/backup
 sleep 1
 
-cp -r /root/.acme.sh /root/backup/ &> /dev/null
+#cp -r /root/.acme.sh /root/backup/ &> /dev/null
 cp -r /etc/passwd /root/backup/ &> /dev/null
 cp -r /etc/group /root/backup/ &> /dev/null
 cp -r /etc/shadow /root/backup/ &> /dev/null
 cp -r /etc/gshadow /root/backup/ &> /dev/null
 cp -r /etc/ppp/chap-secrets /root/backup/chap-secrets &> /dev/null
 cp -r /var/lib/ /root/backup &> /dev/null
-#cp -r /usr/local/etc/xray &> /dev/null
-#cp -r /usr/local/etc/xray backup/xray &> /dev/null
-#cp -r /usr/local/etc/trojan &> /dev/null
-#cp -r /usr/local/etc/vless &> /dev/null
-#cp -r /usr/local/etc/vmess &> /dev/null
 cp -r /etc/xray /root/backup/xray &> /dev/null
 cp -r /etc/nginx/conf.d /root/backup/conf.d/ &> /dev/null
-cp -r /usr/local/shadowsocksr/ /root/backup/shadowsocksr &> /dev/null
 cp -r /home/vps/public_html /root/backup/public_html &> /dev/null
 cp -r /etc/cron.d /root/backup/cron.d &> /dev/null
 cp -r /etc/crontab /root/backup/crontab &> /dev/null
@@ -238,21 +232,18 @@ sleep 1
 cp -r /root/backup/chap-secrets /etc/ppp/ &> /dev/null
 echo -e "[ ${GREEN}INFO${NC} ] • Restoring passwd1 data..."
 sleep 1
-cp -r /root/backup/passwd1 /etc/ipsec.d/passwd &> /dev/null
-echo -e "[ ${GREEN}INFO${NC} ] • Restoring ss.conf data..."
-sleep 1
-cp -r /root/backup/ss.conf /etc/shadowsocks-libev/ss.conf &> /dev/null
-echo -e "[ ${GREEN}INFO${NC} ] • Restoring admin data..."
-sleep 1
+#cp -r /root/backup/passwd1 /etc/ipsec.d/passwd &> /dev/null
+#echo -e "[ ${GREEN}INFO${NC} ] • Restoring ss.conf data..."
+#sleep 1
+#cp -r /root/backup/ss.conf /etc/shadowsocks-libev/ss.conf &> /dev/null
+#echo -e "[ ${GREEN}INFO${NC} ] • Restoring admin data..."
+#sleep 1
 cp -r /root/backup /var/lib/ &> /dev/null
-cp -r /root/backup/wireguard /etc/ &> /dev/null
-cp -r /root/backup/.acme.sh /root/ &> /dev/null
-#cp -r /root/backup/vless /home/ &> /dev/null
-#cp -r /root/backup/trojan /etc/ &> /dev/null
-#cp -r /root/backup/vmess /etc/ &> /dev/null
+#cp -r /root/backup/wireguard /etc/ &> /dev/null
+#cp -r /root/backup/.acme.sh /root/ &> /dev/null
 cp -r /root/backup/xray /etc/ &> /dev/null
 cp -r /root/backup/conf.d /etc/nginx/ &> /dev/null
-cp -r /root/backup/shadowsocksr /usr/local/ &> /dev/null
+#cp -r /root/backup/shadowsocksr /usr/local/ &> /dev/null
 cp -r /root/backup/public_html /home/vps/ &> /dev/null
 cp -r /root/backup/crontab /etc/ &> /dev/null
 cp -r /root/backup/cron.d /etc/ &> /dev/null
