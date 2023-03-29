125d0dad3db6d331c5ede521304b739ff0268211
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-29 19:11:40 +0800

    Update crt.sh

--- tarong/XRAY/crt.sh
+++ tarong/XRAY/crt.sh
@@ -26,3 +26,6 @@ echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
 bash acme.sh --register-account -m merahjambo@gmail.com
 bash acme.sh --issue --standalone -d $domain --force
 bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+systemctl restart nginx
+systemctl restart xray
+menu
